f5ccf09da277a13c4fa80d2491460c9390fbe113
mirmik <mirmikns@yandex.ru>
2023-01-26 18:24:15 +0300

    'a'

--- maho/base/linalg_adaptor.h
+++ maho/base/linalg_adaptor.h
@@ -8,15 +8,16 @@ namespace maho
     using namespace linalg;
 
     template <class T> using vec2 = linalg::vec<T, 2>;
-
     template <class T> using vec3 = linalg::vec<T, 3>;
-
     template <class T> using vec4 = linalg::vec<T, 4>;
-
     template <class T> using mat22 = linalg::mat<T, 2, 2>;
-
+    template <class T> using mat23 = linalg::mat<T, 2, 3>;
+    template <class T> using mat24 = linalg::mat<T, 2, 4>;
+    template <class T> using mat32 = linalg::mat<T, 3, 2>;
     template <class T> using mat33 = linalg::mat<T, 3, 3>;
-
+    template <class T> using mat34 = linalg::mat<T, 3, 4>;
+    template <class T> using mat42 = linalg::mat<T, 4, 2>;
+    template <class T> using mat43 = linalg::mat<T, 4, 3>;
     template <class T> using mat44 = linalg::mat<T, 4, 4>;
 }
 
---
+++ maho/rga3/pose3.h
@@ -0,0 +1,10 @@
+#ifndef MAHO_RGA_POSE3_H
+#define MAHO_RGA_POSE3_H
+
+namespace maho
+{
+    namespace rga
+    {
+
+    }
+}
